008810eb5af0aa20f0d1cb352e7e561cafa3cb87
Stepan <121403794+StepanKoz@users.noreply.github.com>
2023-05-24 14:23:16 +0400

    Add files via upload
    first step of creating tables and small security restrictions

---
+++ Database Project/script.sql
@@ -0,0 +1,14 @@
+
+create table user(user_id serial primary key, surname varchar(100) not null, name varchar(100) not null, phone varchar(100), telegram varchar(100) not null, address varchar(100), email varchar(100) not null);
+
+create table item(item_id serial primary key, name varchar(100) not null, category varchar(100) not null, price real not null, quantity int not null, author varchar(100) not null, size varchar(100) not null, color varchar(100));
+
+create type status_enum as enum('received', 'preparing', 'delivering', 'delivered', 'closed' );
+
+create table order(order_id serial primary key, user_id int, sum real, status status_enum, constraint user_id_fk foreign key(user_id) references user(user_id));
+
+create table order_items(order_id int, item_id int, constraint order_id_fk foreign key(order_id) references order(order_id), constraint item_id_fk foreign key(item_id) references item(item_id));
+
+create table cart(user_id int, item_id int, constraint item_id_fk foreign key(item_id) references item(item_id), constraint user_id_fk foreign key(user_id) references user(user_id));
+
+
